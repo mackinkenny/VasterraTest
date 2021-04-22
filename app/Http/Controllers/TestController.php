e6b0dbf3048f29b8ff5e43e9d6136f30c59b3903
Erkin <Mackinkenny@gmail.com>
2021-04-22 14:41:52 +0600

    testapi

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class TestController extends Controller
+{
+    public function testApi() {
+        $posts = json_decode(file_get_contents('https://moskvichmag.ru/wp-json/wp/v2/posts/'));
+
+        foreach ($posts as $post){
+            $post->content->rendered = str_replace('<p>','<Text style={paragraph}>', $post->content->rendered);
+            $post->content->rendered = str_replace('</p>','</Text>', $post->content->rendered);
+            $post->content->rendered = str_replace('<strong>','<Text style={strong}>', $post->content->rendered);
+            $post->content->rendered = str_replace('</strong>','</Text>', $post->content->rendered);
+        }
+        dd($posts);
+    }
+}
